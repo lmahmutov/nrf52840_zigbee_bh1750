57ed811fcd62b2a4593a82a8b59235f764c97c7b
Lenz <lenz1986@gmail.com>
2020-06-03 11:28:19 +0300

    Revert "init"
    This reverts commit 1c3e9cd07a37dc9ff11bf62b2d968bd8b3121f0d.

--- ADC.h
+++ ADC.h
@@ -5,9 +5,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+    
     #include "nrf_drv_saadc.h"
-    void Adc12bitPolledInitialise(void);
-    int16_t GetBatteryVoltage( void );
+		#ifdef DEBUG_MODE
+		#include "SEGGER_RTT.h"
+		#endif
+		void Adc12bitPolledInitialise(void);
+    int16_t GetBatteryVoltage1( void );
     
 #ifdef __cplusplus
 }
